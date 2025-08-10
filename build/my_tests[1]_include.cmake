68a81db02ab169ff3c08445b562a92d0db2d9dfd
vboxuser <vboxuser@Kostas.myguest.virtualbox.org>
2025-08-10 17:07:27 +0200

    Delete useless files, byproducts of compilation/testing

--- build/my_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/vboxuser/Desktop/CppProject/ModernTaskManager/build/my_tests[1]_tests.cmake")
-  include("/home/vboxuser/Desktop/CppProject/ModernTaskManager/build/my_tests[1]_tests.cmake")
-else()
-  add_test(my_tests_NOT_BUILT my_tests_NOT_BUILT)
-endif()
